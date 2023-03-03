cce6a7eedcf64c24a8d1c5b2b0e9fcca427c6de4
Irbah <irbahluthfifah@gmail.com>
2023-03-03 16:16:33 +0700

    v1.1 membuat pengecekan pada file loopinggit.py

--- loopinggit.py
+++ loopinggit.py
@@ -5,3 +5,14 @@ x = 100;
 while i <= x :
     print (i);
     i = i + 1;
+
+
+def cek_ganjil(angka):
+  return angka % 2 != 0 
+
+
+while i <= x :
+  if cek_ganjil(i):
+    print(i)
+  else: 
+    print("bilangan genap")
